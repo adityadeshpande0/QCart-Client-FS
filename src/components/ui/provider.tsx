4782cacd688923f8130fcd970b5eae7602eb2e41
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-27 23:30:34 +0530

    router and redux setup

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,27 @@
 "use client"
 
+import type { ReactNode } from "react"
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode"
 
-export function Provider(props: ColorModeProviderProps) {
+import { Provider as ReduxProvider } from "react-redux"
+import { store } from "@/app/store"
+
+interface Props extends ColorModeProviderProps {
+  children: ReactNode
+}
+
+export function Provider({ children, ...props }: Props) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
+    <ReduxProvider store={store}>
+      <ChakraProvider value={defaultSystem}>
+        <ColorModeProvider {...props}>
+          {children}
+        </ColorModeProvider>
+      </ChakraProvider>
+    </ReduxProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
