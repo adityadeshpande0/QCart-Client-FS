bb08f9ae4b5d57cfae66ca624e20ed327f39475b
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-28 02:14:36 +0530

    register screen comp

---
+++ src/screens/auth/Register.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import TextInputField from "@/components/reusables/input-fields/TextInputField";
+
+const Register: React.FC = () => {
+  return <></>;
+};
+
+export default Register;
